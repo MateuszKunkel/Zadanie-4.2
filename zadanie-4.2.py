9c1da82fa70d41831727930d23fa746fc168f40d
Mateusz Kunkel <matiqk@wp.pl>
2023-01-07 15:29:29 +0100

    add user input request

--- zadanie-4.2.py
+++ zadanie-4.2.py
@@ -1,1 +1,12 @@
-print("hello world")
+def sprawdz_palindrom(str):
+    list1 = []
+    backwards = ''
+
+    for letter in str:
+        list1.append(letter)
+
+    print(list1)
+
+print('wprowadź wyraz:')
+wyraz = input()
+sprawdz_palindrom(wyraz)
