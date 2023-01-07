cbbbd927bdded593138c7df42b35dcecc5f7ea79
Mateusz Kunkel <matiqk@wp.pl>
2023-01-07 15:39:44 +0100

    add temporary functionality check

--- zadanie-4.2.py
+++ zadanie-4.2.py
@@ -1,11 +1,26 @@
 def sprawdz_palindrom(str):
     list1 = []
+    list2 = []
     backwards = ''
 
     for letter in str:
         list1.append(letter)
 
     print(list1)
+
+    for letter in reversed(list1):
+        list2.append(letter)
+    
+    print(list2)
+
+    backwards = ''.join(list2)
+
+    print(backwards)
+
+    #if str == list2:
+    #    return(True)
+    #else
+    #    return(False)
 
 print('wprowadź wyraz:')
 wyraz = input()
