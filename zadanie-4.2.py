46a565c29215d236a1668cdee8a6fb8b39abb9c7
Mateusz Kunkel <matiqk@wp.pl>
2023-01-12 00:19:58 +0100

    simplified palindrome function

--- zadanie-4.2.py
+++ zadanie-4.2.py
@@ -3,18 +3,8 @@ def sprawdz_palindrom(str):
         sprawdza czy wpisany wyraz jest palindromem
         zwraca wartość boolean True jeśli jest, False jest nie jest
     """    
-    list1 = []
-    list2 = []
-    backwards = ''
-
-    for letter in str:
-        list1.append(letter)
-
-    for letter in reversed(list1):
-        list2.append(letter)
-
-    backwards = ''.join(list2)
-
+    backwards = str[::-1]
+    
     return (str == backwards)
 
 print('wprowadź wyraz:')
